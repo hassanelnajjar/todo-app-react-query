cef10274167f90b3417e2b4f1a6ed6f37b645d44
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-05-26 11:54:25 +0300

    add posts-post-addpost

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
+
+const Posts = ({ posts }) => {
+	return posts.map(({ title, content }) => <li key={Date.now()}>{content}</li>);
+};
+const Post = ({ setPosts }) => {
+	const submitButton = (e) => {
+		e.preventDefault();
+		console.log(e.target[0].value, e.target[1].value);
+		setPosts((posts) => [
+			...posts,
+			{ title: e.target[0].value, content: e.target[1].value },
+		]);
+	};
+	return (
+		<form onSubmit={submitButton}>
+			<input placeholder='title' type='text' />
+			<input placeholder='content' type='text' />
+			<input type='submit' value='submit' />
+		</form>
+	);
+};
 const Home = () => {
-	return <h1>Hassan</h1>;
+	const [posts, setPosts] = React.useState([]);
+
+	return (
+		<>
+			<h1>Posts</h1>
+			<p>Posts Count: {posts.length}</p>
+			<Posts posts={posts}></Posts>
+			<Post setPosts={setPosts}></Post>
+		</>
+	);
 };
 
 export default Home;
